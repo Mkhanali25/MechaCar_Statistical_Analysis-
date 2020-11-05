c855342de5e3e6b3f508b7a8e0a728bef7544d14
ALi <Mkhanali25@gmail.com>
2020-11-05 14:54:49 -0600

    new

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,3 +4,6 @@ install.packages("dplyr")
 MechaTable <-read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 lm(mpg ~ .,MechaTable)
 summary(lm(mpg ~ .,MechaTable))
+CoilTable<-read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+summarize <- CoilTable %>% group_by() %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD= sd(PSI), .groups = 'keep') #create summary table with multiple columns
+Lot_Summary <- CoilTable %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD= sd(PSI), .groups = 'keep') #create summary table with multiple columns
